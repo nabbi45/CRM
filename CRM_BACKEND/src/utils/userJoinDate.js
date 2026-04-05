3f45eb37fb475eef2137846f0ac15df3f4a7a367
Naveen Pratap Singh <naveenpratapsingh45@gmail.com>
2026-04-06 05:23:27 +0530

    Fix: Backend crash when fetching chat history due to missing 'new' in Mongoose ObjectId

--- CRM_BACKEND/src/utils/userJoinDate.js
+++ CRM_BACKEND/src/utils/userJoinDate.js
@@ -4,5 +4,5 @@ export const getUserJoinDate = (userId) => {
   if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
     return new Date(0);
   }
-  return mongoose.Types.ObjectId(userId).getTimestamp();
+  return new mongoose.Types.ObjectId(userId).getTimestamp();
 };
